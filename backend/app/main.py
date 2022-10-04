b2a96aa876cffd0288bec27cab2131c24bf65d26
Merle Hoffmann <hoffmannmerle@web.de>
2022-10-04 22:27:36 +0200

    add: SQLAlchemy ORM and guest model

--- backend/app/main.py
+++
@@ -1,9 +0,0 @@
-import uvicorn
-
-from app import app
-from app.endpoints import *  # noqa F403 F401
-
-
-def main():
-    # run server
-    uvicorn.run(app, host="0.0.0.0")
---
+++ backend/main.py
@@ -0,0 +1,16 @@
+import uvicorn
+from sqlalchemy_utils.functions import create_database
+
+from app.db import engine, Base
+from app import app
+from app.endpoints import *  # noqa F403 F401
+from app.models import *  # noqa F403 F401
+
+
+def main():
+    db_url = engine.url
+    create_database(db_url)
+    Base.metadata.create_all()
+
+    # run server
+    uvicorn.run(app, host="0.0.0.0")
