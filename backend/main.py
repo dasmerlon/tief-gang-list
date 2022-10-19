b091e4d4b4447dce98436e98f632e84f2bdee823
Merle Hoffmann <hoffmannmerle@web.de>
2022-10-19 21:29:12 +0200

    add: commandline interface

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,28 @@
 import uvicorn
 from sqlalchemy import text
-from sqlalchemy_utils.functions import create_database
+from sqlalchemy_utils.functions import create_database, database_exists, drop_database
+
+import typer
 
 from app.db import engine, Base
 from app import app, models, endpoints  # noqa F403 F401
 
+cli = typer.Typer()
 
-def main():
-    db_url = engine.url
-    create_database(db_url)
+
+@cli.command()
+def run():
+    # run server
+    uvicorn.run(app, host="0.0.0.0")
+
+
+@cli.command()
+def createdb():
+    if database_exists(engine.url):
+        print("Database already exists.")
+        return
+
+    create_database(engine.url)
 
     with engine.connect() as connection:
         connection.execute(text('create extension if not exists "uuid-ossp";'))
@@ -16,8 +30,14 @@ def main():
 
     Base.metadata.create_all()
 
-    # run server
-    uvicorn.run(app, host="0.0.0.0")
+
+@cli.command()
+def dropdb():
+    if not database_exists(engine.url):
+        print("Database doesn't exists.")
+        return
+
+    drop_database(engine.url)
 
 
-main()
+cli()
