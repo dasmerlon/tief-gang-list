599db527e1aff7cbd28a719b9fbf11cb56ed10bf
Merle Hoffmann <hoffmannmerle@web.de>
2022-10-27 23:32:04 +0200

    add: missing crud routes for guest

--- backend/app/crud/guest.py
+++ backend/app/crud/guest.py
@@ -1,12 +1,59 @@
 from uuid import UUID
 
 from fastapi_sqlalchemy import db
+from sqlalchemy import func
 
-from app import models
+from app import models, schemas
+
+
+def create(guest: schemas.GuestCreate) -> models.Guest:
+    db_guest = models.Guest(
+        first_name=guest.first_name,
+        last_name=guest.last_name,
+        buddy=guest.buddy,
+        email=guest.email,
+        subscribed=guest.subscribed,
+    )
+
+    db.session.add(db_guest)
+    db.session.commit()
+    return db_guest
 
 
 def get(guest_id: UUID) -> models.Guest | None:
     return db.session.query(models.Guest).get(guest_id)
+
+
+def get_list(
+    first_name_start: str | None,
+    last_name_start: str | None,
+    buddy: str | None,
+    subscribed: bool | None,
+) -> list[models.Guest]:
+    query = db.session.query(models.Guest)
+
+    if first_name_start is not None:
+        query = query.filter(
+            func.lower(models.Guest.first_name).startswith(func.lower(first_name_start))
+        )
+
+    if last_name_start is not None:
+        query = query.filter(
+            func.lower(models.Guest.last_name).startswith(func.lower(last_name_start))
+        )
+
+    if buddy is not None:
+        query = query.filter(models.Guest.buddy == buddy)
+
+    if subscribed is not None:
+        query = query.filter(models.Guest.subscribed == subscribed)
+
+    return query.all()
+
+
+def update(guest_id: UUID, guest_update: schemas.GuestUpdate) -> models.Guest:
+    changes = guest_update.dict(exclude_unset=True)
+    db.session.query(models.Guest).filter(models.Guest.id == guest_id).update(changes)
 
 
 def delete(guest_id: UUID):
--- backend/app/endpoints/guest.py
+++ backend/app/endpoints/guest.py
@@ -5,13 +5,45 @@ from fastapi import status
 from app import app, crud, schemas
 
 
+@app.post(
+    "/guest",
+    response_model=schemas.Guest,
+    status_code=status.HTTP_201_CREATED,
+)
+async def create_guest(guest: schemas.GuestCreate):
+    return crud.guest.create(guest)
+
+
+@app.get(
+    "/guest/list",
+    response_model=list[schemas.Guest],
+    status_code=status.HTTP_200_OK,
+)
+async def get_list(
+    first_name_start: str | None = None,
+    last_name_start: str | None = None,
+    buddy: str | None = None,
+    subscribed: bool | None = None,
+):
+    return crud.guest.get_list(first_name_start, last_name_start, buddy, subscribed)
+
+
 @app.get(
     "/guest/{guest_id}",
     response_model=schemas.Guest,
     status_code=status.HTTP_200_OK,
 )
-async def read_guest(guest_id: UUID):
+async def get_guest(guest_id: UUID):
     return crud.guest.get(guest_id)
+
+
+@app.put(
+    "/guest/{guest_id}",
+    response_model=schemas.Guest,
+    status_code=status.HTTP_200_OK,
+)
+async def update_guest(guest_id: UUID, guest_update: schemas.GuestUpdate):
+    return crud.guest.update(guest_id, guest_update)
 
 
 @app.delete(
