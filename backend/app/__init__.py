63c59caafb77d7e29bb7cf1a2c5baaa012bef094
Merle Hoffmann <hoffmannmerle@web.de>
2022-11-25 17:35:14 +0100

    add: API docs configuration

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,6 +3,6 @@ from fastapi_sqlalchemy import DBSessionMiddleware
 
 from app.db import engine
 
-app = FastAPI()
+app = FastAPI(title="tief-gang-list", version="0.0.1", description="A cool gang. 😎")
 
 app.add_middleware(DBSessionMiddleware, custom_engine=engine)
