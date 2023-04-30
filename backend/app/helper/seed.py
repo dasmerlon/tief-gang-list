dca8dcec731e09da1b3ac2f6f386586e57a26b01
Arne Beer <contact@arne.beer>
2023-04-30 17:49:20 +0200

    add: More seed data

--- backend/app/helper/seed.py
+++ backend/app/helper/seed.py
@@ -1,3 +1,4 @@
+import random
 from datetime import date, datetime, time
 
 from dateutil.relativedelta import relativedelta
@@ -5,7 +6,7 @@ from dateutil.rrule import MONTHLY, SA, rrule
 from faker import Faker
 from sqlalchemy.orm import Session
 
-from app import models
+from app import models, schemas
 from app.db import engine as db_engine
 
 
@@ -39,6 +40,9 @@ def seed_db():
         rrule(MONTHLY, count=len(event_names), byweekday=SA(1), dtstart=months_ago)
     )
 
+    # Create a few random guests
+    guests = create_guests(session, fake)
+
     # Create a few events
     for event_name, event_date in zip(event_names, dates):
         event = models.Event(
@@ -49,5 +53,66 @@ def seed_db():
 
         session.add(event)
         session.flush()
+        # Create some registrations and entry events for this event
+        entities_for_event_and_guests(session, fake, event, guests)
 
     session.commit()
+
+
+def create_guests(session: Session, fake: Faker) -> list[models.Guest]:
+    """Create a list of random guests."""
+    guests = []
+
+    for _ in range(10):
+        guest = models.Guest(
+            first_name=fake.first_name(),
+            last_name=fake.last_name(),
+            buddy="crew",
+            email=fake.ascii_free_email(),
+            subscribed=fake.boolean(),
+        )
+
+        session.add(guest)
+        session.flush()
+        guests.append(guest)
+
+    return guests
+
+
+def entities_for_event_and_guests(
+    session: Session, fake: Faker, event: models.Event, guests: list[models.Guest]
+) -> list[models.Guest]:
+    """Create Registration and EntryEvent entities.
+
+    This function selects a few guests from the list and creates registrations
+    and EntryEvents for these guests for the given event
+    """
+
+    # Select a random set of variable size from our guest list
+    selected_guests = random.sample(guests, random.randint(1, len(guests)))
+
+    for guest in selected_guests:
+        # Register the guest for the event
+        registration = models.Registration(
+            guest_id=guest.id, event_id=event.id, arrived=True
+        )
+        session.add(registration)
+
+        # The guest showed up to the event
+        entry_event = models.EntryEvent(
+            event_id=event.id,
+            event_type=schemas.EntryEventType.arrival,
+        )
+        session.add(entry_event)
+
+        # 50/50 chance that the guest was registered when they left
+        if fake.boolean():
+            entry_event = models.EntryEvent(
+                event_id=event.id,
+                event_type=schemas.EntryEventType.departure,
+            )
+            session.add(entry_event)
+
+    session.flush()
+
+    return guests
