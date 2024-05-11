98dacdca8c5993689ca00505b718d0728e6ad3b4
Arne Beer <contact@arne.beer>
2024-05-11 14:53:43 +0200

    fix: Stuff

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,6 +1,6 @@
-from .entry_event import EntryEvent, EntryEventCreate, EntryEventType, GuestsOnSite
 from .event import Event, EventCreate, EventUpdate
 from .guest import Guest, GuestCreate, GuestUpdate
+from .entry_event import EntryEvent, EntryEventCreate, EntryEventType, GuestsOnSite
 from .registration import (
     Registration,
     RegistrationCreate,
